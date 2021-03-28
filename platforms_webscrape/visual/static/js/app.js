3b20888a7c7e3920bcc5453897bd1dc13aae2942
Chandler Gibbons <chandlergibbons6@gmail.com>
2021-03-27 23:33:15 -0400

    chandler data visual complete

--- platforms_webscrape/visual/static/js/app.js
+++ platforms_webscrape/visual/static/js/app.js
@@ -1,7 +1,32 @@
 var filter = d3.select("#selDataset"); 
 
 // gives a starting reference for display on load
-var samp = "2021"
+var samp = "2016"
+
+
+d3.json("platforms_whole.json").then((data) => {
+    // Creates variables to hold the peices of data. 
+    
+    for (i = 0; i < data.length; i++) {
+        var shows = data[i]
+        
+        show_years.push(shows[2]);
+      }
+     
+
+    // grabs data for drop down
+    var valfilt = show_years.filter((value, index, self) => self.indexOf(value) === index);
+    var valdrop = valfilt.sort((a, b) => a - b);
+
+
+    // creates drop down
+    valdrop.forEach(function (values) {
+            
+        filter.append('option').text(values);
+    
+    });
+
+});
 
 // handles changes
 function optionChanged(sample) {
@@ -34,12 +59,20 @@ function renderProccess(){
     imdbtop =[];
     reeltop = [];
 
-    
+    full_filter_imdb = [];
+    full_filter_reel = [];
+
+    imdb_top_titles = [];
+    reel_top_titles = [];
+
+    imdb_top_plat_disp = [];
+    reel_top_plat_disp = [];
     
         // do I need a delete existing graph line here????
 
     d3.html('');
-
+        
+        
     d3.json("platforms_whole.json").then((data) => {
         // Creates variables to hold the peices of data. 
         
@@ -51,20 +84,6 @@ function renderProccess(){
          
 
         
-
-      
-
-        // grabs data for drop down
-        var valfilt = show_years.filter((value, index, self) => self.indexOf(value) === index);
-        var valdrop = valfilt.sort((a, b) => a - b);
-      
-
-        
-    
-        // creates drop down
-        valdrop.forEach(function (values) {
-            filter.append('option').text(values);
-        });
 
 
         // finds indexes for year selected
@@ -93,7 +112,16 @@ function renderProccess(){
             onhulu.push(values[8]); 
         });
 
-        
+        // get the total number of shows per platform for that year
+        const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
+        prime_tot = countOccurrences(onprime, true)
+        netflix_tot = countOccurrences(onnetflix, true)
+        disney_tot = countOccurrences(ondisney, true)
+        hulu_tot = countOccurrences(onhulu, true)
+
+
+        console.log(prime_tot);
 
         // function to get top ten scores and there index's
         
@@ -112,44 +140,110 @@ function renderProccess(){
 
         
         // get indices of 10 greatest imdb elements
-        var indices = findIndicesOfMax(imdb_ratings, 10);
-        console.log(indices);
+        var imdb_ind = findIndicesOfMax(imdb_ratings, 10);
+        // console.log(imdb_ind);
 
         // get the 10 greatest imdb scores
-        for (var i = 0; i < indices.length; i++)
-            imdbtop.push(imdb_ratings[indices[i]]);
+        for (var i = 0; i < imdb_ind.length; i++)
+            imdbtop.push(imdb_ratings[imdb_ind[i]]);
         
-        console.log(imdbtop);
+        // console.log(imdbtop);
 
-        // get indices of 10 greatest imdb elements
-        var indices = findIndicesOfMax(reel_ratings, 10);
-        console.log(indices);
+        // get indices of 10 greatest reel elements
+        var reel_ind = findIndicesOfMax(reel_ratings, 10);
+        // console.log(reel_ind);
 
-        // get the 10 greatest imdb scores
-        for (var i = 0; i < indices.length; i++)
-            reeltop.push(reel_ratings[indices[i]]);
+        // get the 10 greatest reel scores
+        for (var i = 0; i < reel_ind.length; i++)
+            reeltop.push(reel_ratings[reel_ind[i]]);
         
-        console.log(reeltop);
+        // console.log(reeltop);
 
 
+        // get the full data for top ten reel scores
+
+        reel_ind.forEach(function (values){
+            full_filter_reel.push(data_sel[values]);
+        });
+
+        // get the full data for top ten imdb scores
+
+        imdb_ind.forEach(function (values){
+            full_filter_imdb.push(data_sel[values]);
+        });
+
+        console.log(full_filter_imdb);
+        
+        // get the titles and platform labels for the imdb graph
+
+        full_filter_imdb.forEach(function (values){
+            imdb_top_titles.push(values[1]);
 
 
-        var resultArray = samples.filter(sampleObj => sampleObj.id == samp);
+                var platdisplay = "";
+                
+                if (values[5] == true){
+                    platdisplay += "prime ";
+                }
+
+                else if (values[6] == true){
+                    platdisplay += "netflix ";
+                }
+
+                else if (values[7] == true){
+                    platdisplay += "disney+ ";
+                }
+
+                else if (values[8] == true){
+                    platdisplay += "hulu ";
+                }
+
+           
+         imdb_top_plat_disp.push(platdisplay);
+        });
+
+        // get the titles and platform labels for the reel graph
+
+        full_filter_reel.forEach(function (values){
+            reel_top_titles.push(values[1]);
 
 
-        // adds the first sample to a variable
-        var result = resultArray[0];
+                var platdisplay = "";
+                
+                if (values[5] == true){
+                    platdisplay += "prime ";
+                }
 
-        // gathers variables that hold the chart value parts
-        var otu_ids = result.otu_ids;
-        var sample_values = result.sample_values;
-        var otu_labels = result.otu_labels;
+                else if (values[6] == true){
+                    platdisplay += "netflix ";
+                }
 
-        // reformats the x, y ticks and labels for the bar chart in decending order and adds the OTU to the y ticks
+                else if (values[7] == true){
+                    platdisplay += "disney+ ";
+                }
 
-        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-        var xticks = sample_values.slice(0, 10).reverse();
-        var labels = otu_labels.slice(0, 10).reverse();
+                else if (values[8] == true){
+                    platdisplay += "hulu ";
+                }
+
+           
+         reel_top_plat_disp.push(platdisplay);
+        });
+        
+        console.log(reel_top_titles);
+        console.log(reel_top_plat_disp);
+
+
+        
+
+
+        // IMDB CHART BUILD
+
+        // formats the x, y ticks and labels for the imdb bar chart 
+
+        var yticks = imdb_top_titles;
+        var xticks = imdbtop;
+        var labels = imdb_top_plat_disp;
 
         console.log(yticks);
         console.log(xticks);
@@ -166,77 +260,62 @@ function renderProccess(){
         var data = [trace1] 
 
         var layout = {
-            title: 'top 10 OTUs Found'
+            title: `top 10 IMDB Rated Shows For ${samp}`,
+            xaxis: {
+                tickmode: "linear",
+                tick0: 0.25,
+                dtick: 0.25
+              },
+            yaxis: {
+                automargin: true
+            }
         };
         
         Plotly.newPlot('bar', data, layout);
 
+        // REEL CHART BUILD
 
-        // bubble chart generation
+        // formats the x, y ticks and labels for the Reel bar chart 
 
-        // grabs data
-        // var xticksbub = otu_ids.slice(0, 10);
-        // xticksbub.push.apply(xticksbub, otu_ids.slice(Math.max(otu_ids.length - 10, 1)));
+        var yticks = reel_top_titles;
+        var xticks = reeltop;
+        var labels = reel_top_plat_disp;
 
-        // var yticksbub = sample_values.slice(0, 10);
-        // yticksbub.push.apply(yticksbub, sample_values.slice(Math.max(sample_values.length - 10, 1)));
+        console.log(yticks);
+        console.log(xticks);
+        console.log(labels);
 
-        // var labelsbub = otu_labels.slice(0, 10);
-        // labelsbub.push.apply(labelsbub, otu_labels.slice(Math.max(otu_labels.length - 10, 1)));
+        var trace1 = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            orientation: 'h',
+            type: 'bar'
+        };
+
+        var data = [trace1] 
+
+        var layout = {
+            title: `top 10 Reel Rated Shows For ${samp}`,
+            xaxis: {
+                tickmode: "linear",
+                tick0: 0,
+                dtick: 5
+              },
+            
+            yaxis: {
+                automargin: true
+            }
+        };
+        
+        Plotly.newPlot('bar2', data, layout);
 
 
-        // console.log(xticksbub);
-        // console.log(labelsbub);
-        // console.log(yticksbub);
 
-        // // plots data
-        // var trace1 = {
-        //     x: xticksbub,
-        //     y: yticksbub,
-        //     mode: 'markers',
-        //     marker: {
-        //       color: xticksbub,  
-        //       size: yticksbub,
-        //       colorscale: "Earth"
-        //     },
-        //     text: labelsbub
-        //   };
-          
-        //   var data = [trace1];
-          
-        //   var layout = {
-        //     title: 'First Ten and Last Ten Samples',
-        //     xaxis: {
-        //         title: "OTU ID"
-        //     },
-        //     showlegend: false,
-        //     height: 600,
-        //     width: 1200
-        //   };
-          
-        //   Plotly.newPlot('bubble', data, layout);
-
-        //   demographic chart
-
-        // grabs the metadata based on input
-        var resultArray2 = metadata.filter(metaObj => metaObj.id == samp);
-
-        console.log(resultArray2);
-
-        var result2 = resultArray2[0];
-
-        // parses out all demografic variables
-
-        var age = result2.age;
-        var bbtype = result2.bbtype;
-        var ethnicity = result2.ethnicity;
-        var gender = result2.gender;
-        var id = result2.id;
-        var location = result2.location;
-        var wfreq = result2.wfreq;
+        // PLATFORMS TOTAL CHART
 
         // creates the readable formating
-        var demoinfo = ` id: ${id} <br> ethnicity: ${ethnicity} <br> gender: ${gender} <br> age: ${age} <br> location: ${location} <br> bbtype: ${bbtype} <br> wfreq: ${wfreq}`
+        var demoinfo = ` Prime: ${prime_tot} <br> Netflix: ${netflix_tot} <br> Disney+: ${disney_tot} <br> Hulu: ${hulu_tot}`
 
         document.getElementById('sample-metadata').innerHTML = demoinfo;
 
